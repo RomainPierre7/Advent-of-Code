013e37ce6ad75a8a2185393d3a887d3277441db3
Romain PIERRE <romain.pierre@enseirb-matmeca.fr>
2023-12-02 09:28:12 +0100

    script

---
+++ make_day.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 year day"
+    exit 1
+fi
+
+year="$1"
+day="$2"
+
+if [ ! -d "$year" ]; then
+    mkdir "$year"
+    echo "Created directory $year"
+fi
+
+if [ ! -d "$year/day$day" ]; then
+    mkdir "$year/day$day"
+    echo "Created directory day$day"
+fi
+
+if [ ! -f "$year/day$day/${day}_input.txt" ]; then
+    touch "$year/day$day/${day}_input.txt"
+    echo "Created file ${day}_input.txt"
+fi
+
+if [ ! -f "$year/day$day/${day}_1.py" ]; then
+    echo "import os
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_directory, '${day}_input.txt')
+input = open(file_path, 'r')" > "$year/day$day/${day}_1.py"
+    echo "Created file ${day}_1.py"
+fi
+
+if [ ! -f "$year/day$day/${day}_2.py" ]; then
+    echo "import os
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_directory, '${day}_input.txt')
+input = open(file_path, 'r')" > "$year/day$day/${day}_2.py"
+    echo "Created file ${day}_2.py"
+fi
+
+echo "Done !"
