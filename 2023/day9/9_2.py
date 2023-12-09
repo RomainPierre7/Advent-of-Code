6edacbed0bc6ae9c4163ddf6e5ca5256d2db7509
Romain PIERRE <romain.pierre@enseirb-matmeca.fr>
2023-12-09 16:33:23 +0100

    day 9

---
+++ 2023/day9/9_2.py
@@ -0,0 +1,35 @@
+import os
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_directory, '9_input.txt')
+input_file = open(file_path, 'r')
+
+histories = []
+for line in input_file:
+    histories.append([int(value) for value in line.split()])
+
+def is_all_zeroes(sequence):
+    for num in sequence:
+        if num != 0:
+            return False
+    return True
+
+def next_sequence(sequence):
+    next = []
+    for i in range (1, len(sequence)):
+        next.append(sequence[i] - sequence[i-1])
+    return next
+
+result = 0
+
+for history in histories:
+    sequences = [history]
+    while not is_all_zeroes(sequences[-1]):
+        sequences.append(next_sequence(sequences[-1]))
+    
+    sequences[-1] = [0] + sequences[-1]
+    for i in range(len(sequences) - 2, -1, -1):
+        sequences[i] = [sequences[i][0] - sequences[i+1][0]] + sequences[i]
+    result += sequences[0][0]
+
+print(result)
