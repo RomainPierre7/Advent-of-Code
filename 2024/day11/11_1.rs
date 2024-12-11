cf531b89767fc8ca0c0532f4ee381ca567918116
Romain PIERRE <romain.pierre@enseirb-matmeca.fr>
2024-12-11 12:05:50 +0100

    Memoization

--- 2024/day11/11_1.rs
+++ 2024/day11/11_1.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -21,11 +22,7 @@ fn main() -> io::Result<()> {
         }
     }
 
-    for _ in 0..25 {
-        blink(&mut data);
-    }
-
-    let res = data.len();
+    let res = blink(&data, 25);
 
     println!("{res}");
 
@@ -40,18 +37,38 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn blink(data: &mut Vec<u64>) {
-    for i in 0..data.len() {
-        if data[i] == 0 {
-            data[i] = 1
-        } else if data[i].to_string().len() % 2 == 0 {
-            let str = data[i].to_string();
-            let str1 = &str[..str.len() / 2];
-            let str2 = &str[str.len() / 2..];
-            data[i] = str1.parse().expect("Failed to convert");
-            data.push(str2.parse().expect("Failed to convert"));
-        } else {
-            data[i] *= 2024;
+fn blink(data: &Vec<u64>, times: u32) -> u64 {
+    fn count(num: u64, depth: u32, cache: &mut HashMap<(u64, u32), u64>) -> u64 {
+        if depth == 0 {
+            return 1;
         }
+
+        if let Some(&cached_result) = cache.get(&(num, depth)) {
+            return cached_result;
+        }
+
+        let result = if num == 0 {
+            count(1, depth - 1, cache)
+        } else if num.to_string().len() % 2 == 0 {
+            let str_num = num.to_string();
+            let mid = str_num.len() / 2;
+            let s1: u64 = str_num[..mid].parse().expect("Failed to convert to u64");
+            let s2: u64 = str_num[mid..].parse().expect("Failed to convert to u64");
+            count(s1, depth - 1, cache) + count(s2, depth - 1, cache)
+        } else {
+            count(num * 2024, depth - 1, cache)
+        };
+
+        cache.insert((num, depth), result);
+        return result;
     }
+
+    let mut cache: HashMap<(u64, u32), u64> = HashMap::new();
+    let mut res: u64 = 0;
+
+    for &value in data.iter() {
+        res += count(value, times, &mut cache);
+    }
+
+    return res;
 }
