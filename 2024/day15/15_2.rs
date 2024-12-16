7b875fc1d01ed927ca9904680496f805784dc33f
Romain PIERRE <romain.pierre@enseirb-matmeca.fr>
2024-12-16 13:53:32 +0100

    Day 15 p2 in progress

--- 2024/day15/15_2.rs
+++ 2024/day15/15_2.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -14,7 +15,16 @@ fn main() -> io::Result<()> {
                 if content.starts_with('#') {
                     let mut vec: Vec<char> = Vec::new();
                     for c in content.chars() {
-                        vec.push(c);
+                        if c == '@' {
+                            vec.push(c);
+                            vec.push('.');
+                        } else if c == 'O' {
+                            vec.push('[');
+                            vec.push(']');
+                        } else {
+                            vec.push(c);
+                            vec.push(c);
+                        }
                     }
                     grid.push(vec);
                 } else {
@@ -29,9 +39,9 @@ fn main() -> io::Result<()> {
     let heigth = grid.len();
     let width = grid[0].len();
 
-    //println!("{:?}", instructions);
-    //print(grid.clone());
-    //println!();
+    println!("{:?}", instructions);
+    print(grid.clone());
+    println!();
 
     let mut robot = (0, 0);
 
@@ -45,9 +55,9 @@ fn main() -> io::Result<()> {
 
     for instr in instructions {
         make_step(&mut grid, &mut robot, instr);
-        //println!("{instr}");
-        //print(grid.clone());
-        //println!();
+        println!("{instr}");
+        print(grid.clone());
+        println!();
     }
 
     let res = gps(grid);
@@ -89,19 +99,13 @@ fn make_step(grid: &mut Vec<Vec<char>>, robot: &mut (usize, usize), instr: char)
         robot.0 = new_robot.0;
         robot.1 = new_robot.1;
     } else {
-        let mut o_pos = new_robot;
-        while grid[o_pos.0][o_pos.1] != '#' {
-            if grid[o_pos.0][o_pos.1] == '.' {
-                grid[new_robot.0][new_robot.1] = '@';
-                grid[robot.0][robot.1] = '.';
-                grid[o_pos.0][o_pos.1] = 'O';
-                robot.0 = new_robot.0;
-                robot.1 = new_robot.1;
-                break;
-            }
-
-            o_pos.0 = (o_pos.0 as isize + dir.0) as usize;
-            o_pos.1 = (o_pos.1 as isize + dir.1) as usize;
+        let linked_boxes = get_linked_boxes(*robot, dir, grid);
+        if bloc_is_movable(&linked_boxes, dir, grid) {
+            move_bloc(*robot, &linked_boxes, dir, grid);
+            grid[new_robot.0][new_robot.1] = '@';
+            grid[robot.0][robot.1] = '.';
+            robot.0 = new_robot.0;
+            robot.1 = new_robot.1;
         }
     }
 }
@@ -126,5 +130,88 @@ fn gps(grid: Vec<Vec<char>>) -> u64 {
 fn print(grid: Vec<Vec<char>>) {
     for i in 0..grid.len() {
         println!("{:?}", grid[i]);
+    }
+}
+
+fn get_linked_boxes(
+    robot: (usize, usize),
+    dir: (isize, isize),
+    grid: &Vec<Vec<char>>,
+) -> HashSet<(usize, usize)> {
+    let mut linked_boxes: HashSet<(usize, usize)> = HashSet::new();
+
+    let mut last_added: HashSet<(usize, usize)> = HashSet::new();
+    last_added.insert(robot);
+
+    fn rec(
+        last_added: HashSet<(usize, usize)>,
+        linked_boxes: &mut HashSet<(usize, usize)>,
+        dir: (isize, isize),
+        grid: &Vec<Vec<char>>,
+    ) {
+        let mut new_added: HashSet<(usize, usize)> = HashSet::new();
+
+        for pos in last_added {
+            let new_pos = (
+                (pos.0 as isize + dir.0) as usize,
+                (pos.1 as isize + dir.1) as usize,
+            );
+            if grid[new_pos.0][new_pos.1] == '[' {
+                linked_boxes.insert(new_pos);
+                linked_boxes.insert((new_pos.0, new_pos.1 + 1));
+                new_added.insert(new_pos);
+            } else if grid[new_pos.0][new_pos.1] == ']' {
+                linked_boxes.insert(new_pos);
+                linked_boxes.insert((new_pos.0, new_pos.1 - 1));
+                new_added.insert(new_pos);
+            }
+        }
+
+        if new_added.len() > 0 {
+            rec(new_added, linked_boxes, dir, grid);
+        }
+    }
+
+    rec(last_added, &mut linked_boxes, dir, grid);
+
+    //println!("Found {} linked boxes", linked_boxes.len());
+
+    return linked_boxes;
+}
+
+fn bloc_is_movable(
+    linked_boxes: &HashSet<(usize, usize)>,
+    dir: (isize, isize),
+    grid: &Vec<Vec<char>>,
+) -> bool {
+    for pos in linked_boxes {
+        let new_pos = (
+            (pos.0 as isize + dir.0) as usize,
+            (pos.1 as isize + dir.1) as usize,
+        );
+        if grid[new_pos.0][new_pos.1] == '#' {
+            println!("Bloc is not movable");
+            return false;
+        }
+    }
+    println!("Bloc is movable");
+    return true;
+}
+
+fn move_bloc(
+    robot: (usize, usize),
+    linked_boxes: &HashSet<(usize, usize)>,
+    dir: (isize, isize),
+    grid: &mut Vec<Vec<char>>,
+) {
+    let grid_copy = grid.clone();
+
+    for pos in linked_boxes {
+        let new_pos = (
+            (pos.0 as isize + dir.0) as usize,
+            (pos.1 as isize + dir.1) as usize,
+        );
+        grid[new_pos.0][new_pos.1] = grid_copy[pos.0][pos.1];
+        grid[pos.0][pos.1] = '.';
     }
 }
