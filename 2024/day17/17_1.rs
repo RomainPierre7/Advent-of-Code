36e446829ec5b9c7a77d4a65d728cb9b370a6110
Romain PIERRE <romain.pierre@enseirb-matmeca.fr>
2024-12-17 16:14:19 +0100

    Day 17 p1

--- 2024/day17/17_1.rs
+++ 2024/day17/17_1.rs
@@ -5,29 +5,51 @@ use std::path::Path;
 fn main() -> io::Result<()> {
     let path = "17_input.txt";
 
-    let mut data: Vec<Vec<i32>> = Vec::new();
+    let mut program: Vec<i32> = Vec::new();
+    let mut register_a: i32 = 0;
+    let mut register_b: i32 = 0;
+    let mut register_c: i32 = 0;
 
     if let Ok(lines) = read_lines(path) {
         for line in lines {
             if let Ok(content) = line {
-                let mut vec: Vec<i32> = Vec::new();
-                for number in content.split_whitespace() {
-                    if let Ok(num) = number.parse::<i32>() {
-                        vec.push(num);
-                    } else {
-                        eprintln!("Line format incorrect: {}", content);
+                if let Some(value) = content.strip_prefix("Register A: ") {
+                    register_a = value
+                        .trim()
+                        .parse::<i32>()
+                        .expect("Failed to parse Register A value");
+                } else if let Some(value) = content.strip_prefix("Register B: ") {
+                    register_b = value
+                        .trim()
+                        .parse::<i32>()
+                        .expect("Failed to parse Register B value");
+                } else if let Some(value) = content.strip_prefix("Register C: ") {
+                    register_c = value
+                        .trim()
+                        .parse::<i32>()
+                        .expect("Failed to parse Register C value");
+                } else if let Some(value) = content.strip_prefix("Program: ") {
+                    for number in value.trim().split(',') {
+                        if let Ok(num) = number.trim().parse::<i32>() {
+                            program.push(num);
+                        } else {
+                            eprintln!("Failed to parse program number: '{}'", number.trim());
+                        }
                     }
                 }
-                data.push(vec);
             }
         }
     }
 
-    println!("{:?}", data);
+    println!("Register A: {}", register_a);
+    println!("Register B: {}", register_b);
+    println!("Register C: {}", register_c);
+    println!("Program: {:?}", program);
 
     Ok(())
 }
 
+// Função auxiliar para ler linhas de um arquivo
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
     P: AsRef<Path>,
@@ -35,4 +57,3 @@ where
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
-
